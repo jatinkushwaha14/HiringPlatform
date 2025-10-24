d92f6bd5861d403a26097e15132905a306645654
jatinkushwaha14 <jatinkushwaha14@gmail.com>
2025-10-24 18:41:53 +0530

    add working jobs list

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,16 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import TypeTest from './components/TypeTest';
-import JobsTest from './components/JobsTest';
+import JobsList from './components/Jobs/JobsList';
 import './App.css';
 
 function App() {
   return (
     <Provider store={store}>  
       <div className="App">
-        <h1>Hello World!</h1>
-        <p>Basic React app is working!</p>
+        <h1>TalentFlow - Hiring Platform</h1>
         <TypeTest />
-        <JobsTest />
+        <JobsList />
       </div>
     </Provider>
   );
